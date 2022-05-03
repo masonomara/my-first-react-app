f4f051f0635c81167c4373f3f2b1e6f5a5a4b03a
Mason O'Mara <mason.omara@gmail.com>
2022-05-03 19:17:11 -0400

    created a modal

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
 import './App.css';
 import { useState } from 'react';
+import Title from './components/Title';
+import Modal from './components/Modal';
 
 function App() {
-  const [name, setName] = useState("mario")
+  const [showModal, setShowModal] = useState(true)
+  const [showEvents, setShowEvents] = useState(true)
   const [events, setEvents] = useState([
     {title: "mario's birthday bash", id: 1},
     {title: "bowsers' live stream", id: 2},
     {title: "race on moo moo farm", id: 3}
   ])
   
-  const handleClick = () => {
-    setName("luigi")
-    console.log(name)
+  console.log(showModal)
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id
+      })
+    })
+    console.log(id)
+  }
+
+  const handleClose = () => {
+    setShowModal(false)
   }
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
-      {events.map((event, index) => (
+
+      <Title title="Events in Your area"/>
+
+      {showEvents && (
+        <>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
+        </>
+      )}
+      {!showEvents && (
+        <>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </>
+      )}
+      {showEvents && events.map((event, index) => (
         <div key={event.id}>
           <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
         </div>
       ))}
+
+      {showModal && <Modal handleClose={handleClose}>
+        <h2>10% Off Coupon Code!!</h2>
+        <p>Use the code NINJA10 at the checkout.</p>
+      </Modal>}
+
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+export default function Title({ title, subtitle }) {
+  return (
+      <>
+          <h1 className="title">{title}</h1>
+          <br />
+          <h2 className="subtitle">All the latest events in mario kingdom</h2>
+      </>
+  )
+}
